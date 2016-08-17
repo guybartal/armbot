7a7fe78ed64bca4c5af866562012b67422646e44
Guy Bertental <guybartal@gmail.com>
2016-08-17 18:25:27 +0300

    keys

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MICROSOFT_APP_ID || 'e93fe12a-9df2-4060-a2e8-4a50607275f1',
+    appPassword: process.env.MICROSOFT_APP_PASSWORD || 'QCYez4ubf2mNxbdP2ZhuWz1'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
